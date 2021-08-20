553a8e193e465e345e84022d448940136ef76434
BugbearR <bugbearr@bugbearr.jp>
2021-08-20 18:35:20 +0900

    Add stdint.h (empty)

---
+++ include/stdint.h
@@ -0,0 +1,11 @@
+/**
+ * @file stdint.h
+ * @author BugbearR (bugbearr@gmail.com)
+ * @brief Standard integer header
+ * @version 0.1.0
+ * @date 2021-08-19
+ */
+#ifndef _BBRLIBSTDC_STDINT_H_
+#define _BBRLIBSTDC_STDINT_H_
+
+#endif /* _BBRLIBSTDC_STDINT_H_ */
